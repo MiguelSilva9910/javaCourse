26e89d10c78b3dcd85f1e173aef1f0f76c8db447
Miguel <miguel.silva@school.mindera.com>
2019-09-07 17:39:54 +0100

    Exercise 1, 2 and 3 -> prints

--- src/exercises/Exercise1And2.java
+++ src/exercises/Exercise1And2.java
@@ -1,9 +1,35 @@
 package exercises;
-
 public class Exercise1And2 {
     public static void main(String[] args){
-        System.out.print("-\tMilk\n\t\t>\tSemi-skimmed\n\t\t>\tMedium\n");
-        System.out.print("-\tEgg\n\t\t>\tFree-range\n");
-        System.out.print("-\tBread");
+        Exercise1();
+        Exercise2();
+        Exercise3();
+    }
+
+    public static void Exercise1(){
+        System.out.println("----------------------------|");
+        System.out.println("\t\t\tExercise1");
+        System.out.print("-\tMilk\n\t>\tSemi-skimmed\n\t>\tMedium\n-\tEgg\n\t>\tFree-range\n-\tBread\n");
+        System.out.println("----------------------------|");
+    }
+
+    public static void Exercise2(){
+        System.out.println("\t\t\tExercise2");
+        System.out.println("-\tMilk");
+        System.out.println("\t>\tSemi-skimmed");
+        System.out.println("\t>\tMedium");
+        System.out.println("-\tEgg");
+        System.out.println("\t>\tFree-range");
+        System.out.println("-\tBread");
+        System.out.println("----------------------------|");
+    }
+
+    public static void Exercise3(){
+        System.out.println("\t\t\tExercise3");
+        System.out.print("The\n");
+        System.out.print("\tCake\n");
+        System.out.print("Is\t\t\tA\n");
+        System.out.print("\tLie.\n");
+        System.out.println("----------------------------|");
     }
 }

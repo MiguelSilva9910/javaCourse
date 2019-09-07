2126b632fb91ea48d0ead0bb116c80bd3d2c552c
Miguel <miguel.silva@school.mindera.com>
2019-09-07 16:40:24 +0100

    Exercise1/2/3

---
+++ src/exercises/Exercise1And2.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class Exercise1And2 {
+    public static void main(String[] args){
+        System.out.print("-\tMilk\n\t\t>\tSemi-skimmed\n\t\t>\tMedium\n");
+        System.out.print("-\tEgg\n\t\t>\tFree-range\n");
+        System.out.print("-\tBread");
+    }
+}
